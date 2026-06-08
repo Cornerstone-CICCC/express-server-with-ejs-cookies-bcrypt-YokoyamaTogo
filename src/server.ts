8572578918608cb5c95f5f28b4348cb1d8444805
kirakira <15catinthebox@gmail.com>
2026-06-08 02:43:38 -0700

    register user

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,15 @@ import * as path from "path";
 const app = express();
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
+type User = {
+  email: string;
+  password: string;
+};
+
+const users: User[] = [];
+
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.urlencoded({ extended: true }));
 
 // setting view engine with EJS
 app.set("view engine", "ejs");
@@ -29,6 +37,16 @@ app.get("/register", (req: express.Request, res: express.Response) => {
   res.render("pages/register", {
     tabTitle: "REGISTER PAGE",
   });
+});
+
+app.post("/register", (req: express.Request, res: express.Response) => {
+  const newUser: User = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  users.push(newUser);
+  res.redirect("/login");
 });
 
 app.listen(3000, () => console.log("Server is listening on port 3000"));
